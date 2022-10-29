b96112d680c46a84251038613e2d2df9ae526354
YangYgYang <you@example.com>
2022-10-29 12:01:14 +0800

    add home view page

--- app.js
+++ app.js
@@ -19,63 +19,10 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", (req, res) => {
-    res.sendStatus(200)
-  })
-app.get("/webhook", (req, res) => {
-    res.send("good")
-  })
+const routes = require('./routes')
+app.use(routes)
 
-app.post("/webhook", function(req, res) {
-    res.send('its in post webhook route')
-    if (req.body.events[0].type === "message") {
-        // Message data, must be stringified
-        const dataString = JSON.stringify({
-          replyToken: req.body.events[0].replyToken,
-          messages: [
-            {
-              "type": "text",
-              "text": "Hello, user"
-            },
-            {
-              "type": "text",
-              "text": "May I help you?"
-            }
-          ]
-        })
-    
-        // Request header
-        const headers = {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + TOKEN
-        }
-    
-        // Options to pass into the request
-        const webhookOptions = {
-          "hostname": "api.line.me",
-          "path": "/v2/bot/message/reply",
-          "method": "POST",
-          "headers": headers,
-          "body": dataString
-        }
-    
-        // Define request
-        const request = https.request(webhookOptions, (res) => {
-          res.on("data", (d) => {
-            process.stdout.write(d)
-          })
-        })
-    
-        // Handle error
-        request.on("error", (err) => {
-          console.error(err)
-        })
-    
-        // Send data
-        request.write(dataString)
-        request.end()
-      }
-  })
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
