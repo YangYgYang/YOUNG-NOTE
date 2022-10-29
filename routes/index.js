29f1c0cab8db5f68cd7ffca865abecfcdc1dfef7
YangYgYang <you@example.com>
2022-10-29 13:25:21 +0800

    rebluid line webhook router

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const home = require('./modules/home')
-// const line = require('./modules/line')
+const line = require('./modules/line')
 
-// router.use('/webhook', line)
+router.use('/webhook', line)
 router.use('/', home)
 
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,65 +1,9 @@
 const https = require("https")
 const express = require('express')
 const router = express.Router()
-const TOKEN = process.env.LINE_ACCESS_TOKEN
 
 router.get("/", (req, res) => {
     res.render('index')
-  })
-
-router.get("/webhook", (req, res) => {
-    res.send("good")
-  })
-
-router.post("/webhook", function(req, res) {
-    res.send('its in post webhook route')
-    if (req.body.events[0].type === "message") {
-        // Message data, must be stringified
-        const dataString = JSON.stringify({
-          replyToken: req.body.events[0].replyToken,
-          messages: [
-            {
-              "type": "text",
-              "text": "Hello, user"
-            },
-            {
-              "type": "text",
-              "text": "May I help you?"
-            }
-          ]
-        })
-    
-        // Request header
-        const headers = {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + TOKEN
-        }
-    
-        // Options to pass into the request
-        const webhookOptions = {
-          "hostname": "api.line.me",
-          "path": "/v2/bot/message/reply",
-          "method": "POST",
-          "headers": headers,
-          "body": dataString
-        }
-    
-        // Define request
-        const request = https.request(webhookOptions, (res) => {
-          res.on("data", (d) => {
-            process.stdout.write(d)
-          })
-        })
-    
-        // Handle error
-        request.on("error", (err) => {
-          console.error(err)
-        })
-    
-        // Send data
-        request.write(dataString)
-        request.end()
-      }
   })
 
   module.exports = router
